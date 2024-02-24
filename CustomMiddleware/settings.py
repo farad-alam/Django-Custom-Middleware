3a42624c67709de6ff286e7727c51ecf783b5235
farad-alam <farad.alam1997@gmail.com>
2024-02-24 10:17:16 +0600

    Custom class based middleware work successfully

--- CustomMiddleware/settings.py
+++ CustomMiddleware/settings.py
@@ -48,7 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.middleware.my_fun_middleware'
+    # 'blog.middleware.my_fun_middleware',
+    'blog.middleware.MyClassMiddleware',
 ]
 
 ROOT_URLCONF = 'CustomMiddleware.urls'
--- blog/middleware.py
+++ blog/middleware.py
@@ -1,4 +1,3 @@
-
 #funtion base middleware
 
 def my_fun_middleware(get_response):
@@ -12,3 +11,18 @@ def my_fun_middleware(get_response):
         print("after view code")
         return response
     return my_funtion
+
+#class based middleware
+
+class MyClassMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        print("One time initilization code goes here")
+    def __call__(self, request):
+        # before view code goes here
+        print("before view class bsased ")
+        response =self.get_response(request)
+        # after view code goes here
+        print("after view class bsased ")
+        return response
+    
